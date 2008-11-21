ba903b49becc4f2420f9bc496b347da965d5288c
alex <alex@bff75499-2b28-0410-8f74-d29e96b56331>
2008-11-21 19:52:03 +0000

    small changes, inserting mapping for legacy product infos after changing product model

--- unartig-server/trunk/web/src/main/java/ch/unartig/studioserver/colorplaza/OipsPidMapper.java
+++ unartig-server/trunk/web/src/main/java/ch/unartig/studioserver/colorplaza/OipsPidMapper.java
@@ -16,23 +16,23 @@
  *
  *************************************************
  * $Log$
- * Revision 1.3  2007/03/15 21:45:27  alex
- * no more price segment
- *
- * Revision 1.2  2007/03/14 02:41:02  alex
- * initial checkin
- *
- * Revision 1.1  2007/03/13 23:24:06  alex
- * initial checkin
  *
  ****************************************************************/
 package ch.unartig.studioserver.colorplaza;
 
+import ch.unartig.exceptions.UnartigException;
 import ch.unartig.studioserver.businesslogic.ProductMapperIF;
 import ch.unartig.studioserver.model.Product;
+import ch.unartig.studioserver.model.ProductType;
+import ch.unartig.studioserver.model.Price;
+
+import java.math.BigDecimal;
 
 /**
  * Mapper between unartig album products and the colorplaza oips products
+ * TODO this class smells .... all changes in prices / producttypes need also be reflected here
+ *
+ * TODO Have on table productPrices? (prices2producttypes ....)
  */
 public class OipsPidMapper implements ProductMapperIF {
 
@@ -41,8 +41,9 @@ public class OipsPidMapper implements ProductMapperIF {
      * 2-dimensional arrays for producttype-price-oipspid mapping<br/>
      * [ProductTypeId][PriceId]
      */
-    String[][] productPricesOipsid = new String[30][30];
-
+    Object[][] productPricesOipsid = new Object[30][30];
+    private static final int _OIPS_PID_INDEX = 0; // location of the oips pid information in the array
+    private static final int _UNARTIG_EARNINGS_INDEX = 1 ; // index for the position of the unartig earnings value in the array
 
 
     private OipsPidMapper() {
@@ -59,32 +60,124 @@ public class OipsPidMapper implements ProductMapperIF {
 
     /**
      * called only once upon creation. set all static data
+     * [ProductTypeId] [PriceId]
+     * The stored value is a String Array in the form: {"OIPS_PiD","Earnings-Unartig after production/invoicing"}
      */
     private void init() {
-        // 10 x 15 print
-        productPricesOipsid[1][1]= "6301";
-        productPricesOipsid[1][10]= "6501";
         // digifoto 400 x 600 (all digital oipspid)
-        productPricesOipsid[2][3]= "998";
-        productPricesOipsid[2][19]= "998";
-        productPricesOipsid[2][20]= "998";
+        productPricesOipsid[2][19]= new String[] {"998","0.45"};
+        productPricesOipsid[2][22]= new String[] {"998","3.51"};
+        productPricesOipsid[2][28]= new String[] {"998","14.40"};
         // digital negative
-        productPricesOipsid[3][2]= "998";
-        productPricesOipsid[3][5]= "998";
+        productPricesOipsid[3][2]= new String[] {"998","4.95"};
+        productPricesOipsid[3][26]= new String[] {"998","26.55"};
+        productPricesOipsid[3][29]= new String[] {"998","8.91"};
+        // 10 x 15 print
+        productPricesOipsid[4][1]= new String[] {"6301","4.05"};
+        productPricesOipsid[4][10]= new String[] {"6501","8.55"};
+        productPricesOipsid[4][19]= new String[] {"6101","0"};
         // 11 x 17 print
-        productPricesOipsid[4][2]= "6304";
-        productPricesOipsid[4][11]= "6504";
-        // 13 x 19 pring
-        productPricesOipsid[5][3]= "6302";
-        productPricesOipsid[5][12]= "6502";
+        productPricesOipsid[5][2]= new String[] {"6304","4.455"};
+        productPricesOipsid[5][11]= new String[] {"6504","10.305"};
+        productPricesOipsid[5][20]= new String[] {"6104","0"};
+        // 13 x 19 print
+        productPricesOipsid[6][3]= new String[] {"6302","5.31"};
+        productPricesOipsid[6][12]= new String[] {"6502","11.61"};
+        productPricesOipsid[6][21]= new String[] {"6102","0"};
+        // 20 x 30 print
+        productPricesOipsid[7][4]= new String[] {"6303","9.09"};
+        productPricesOipsid[7][13]= new String[] {"6503","21.69"};
+        productPricesOipsid[7][22]= new String[] {"6103","0"};
+        // 30 x 45 print
+        productPricesOipsid[8][5]= new String[] {"6324","10.8"};
+        productPricesOipsid[8][14]= new String[] {"6524","28.8"};
+        productPricesOipsid[8][23]= new String[] {"6124","0"};
+        // 40 x 60 print
+        productPricesOipsid[9][6]= new String[] {"6317","13.5"};
+        productPricesOipsid[9][15]= new String[] {"6517","46.8"};
+        productPricesOipsid[9][24]= new String[] {"6117","0"};
+        // 50 x 70 print
+        productPricesOipsid[10][7]= new String[] {"6310","15.3"};
+        productPricesOipsid[10][16]= new String[] {"6510","55.8"};
+        productPricesOipsid[10][25]= new String[] {"6110","0"};
+        // Mousepad
+
+         // leave this until products 26,134 and 89 are set back to price 9 !!!!! Then delete.
+        productPricesOipsid[12][6]= new String[] {"6316","11.7"};
+        productPricesOipsid[12][15]= new String[] {"6516","20.7"};
+        productPricesOipsid[12][24]= new String[] {"6116","0"};
+        // then delete
+
+//        correct settings:
+        productPricesOipsid[12][9]= new String[] {"6316","11.7"};
+        productPricesOipsid[12][18]= new String[] {"6516","20.7"};
+        productPricesOipsid[12][27]= new String[] {"6116","0"};
+
+//      mapping for old unartig legacy digital products:
+        productPricesOipsid[2][30]= new String[] {"998","2.7"};
+        productPricesOipsid[3][31]= new String[] {"998","26.91"};
+
+
+
     }
 
     /**
      * @see ch.unartig.studioserver.businesslogic.ProductMapperIF
      */
-    public String map(Product product) {
-
-        return productPricesOipsid[product.getProductType().getProductTypeId().intValue()][product.getPrice().getPriceId().intValue()];
+    public String getMappedProductId(Product product)
+    {
+        // todo catch possible out of array exception and re-throw typed exception
+        return getMappedProductId(product.getProductType().getProductTypeId(),product.getPrice().getPriceId());
     }
+
+    public String getMappedProductId(Long productTypeId,Long priceId)
+    {
+        final String[] mappedArray = getMappedArray(productTypeId,priceId);
+        return mappedArray[_OIPS_PID_INDEX];
+
+    }
+    public BigDecimal getUnartigEarnings(Product product) throws UnartigException
+    {
+        return getUnartigEarnings(product.getProductType(),product.getPrice());
+    }
+
+    /**
+     * Delegating method to ch.unartig.studioserver.businesslogic.ProductMapperIF#getUnartigEarnings(java.lang.Long,java.lang.Long)
+     * @see ch.unartig.studioserver.businesslogic.ProductMapperIF#getUnartigEarnings(java.lang.Long,java.lang.Long)
+     * @param productType product type object
+     * @param price price object
+     * @return unartig earnings from colorplaza
+     * @throws UnartigException
+     */
+    public BigDecimal getUnartigEarnings(ProductType productType, Price price) throws UnartigException
+    {
+        return getUnartigEarnings(productType.getProductTypeId(),price.getPriceId());
+    }
+
+
+    public BigDecimal getUnartigEarnings(Long productTypeId, Long priceId) throws UnartigException
+    {
+        final String string;
+        BigDecimal retVal;
+        try
+        {
+            final String[] mappedArray= getMappedArray(productTypeId,priceId);
+            string = mappedArray[_UNARTIG_EARNINGS_INDEX];
+
+            retVal = new BigDecimal(string);
+        } catch (Exception e)
+        {
+           throw new UnartigException("Error getting the mapped values",e);
+        }
+        return retVal;
+    }
+
+
+    private String[] getMappedArray(final Long productTypeId, final Long priceId)
+    {
+        return (String[])productPricesOipsid[productTypeId.intValue()][priceId.intValue()];
+    }
+
+
 
 }
